8b6beff1575f899dc0f0c79207d613d46bb39a25
Kauã Barros <kauabarrostrabalho@gmail.com>
2025-01-28 19:05:07 -0300

    feat: adição, diminuição e exclusão de produtos ao carrinho

--- src/components/CarrinhoSuspenso/ItemCarrinhoSuspenso/index.jsx
+++ src/components/CarrinhoSuspenso/ItemCarrinhoSuspenso/index.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Quantidade from "@/components/Quantidade";
 import Botao from "@/components/Botao";
 import ValorFormatado from "@/components/ValorFormatado";
+import { useCarrinhoContext } from "@/hooks/useCarrinhoContext";
 
-const ItemCarrinhoSuspenso = ({
-  itemCarrinho,
-  removerProdutoCarrinho,
-  adicionarProduto,
-  removerProduto,
-}) => {
+const ItemCarrinhoSuspenso = ({ itemCarrinho }) => {
+  const { adicionarProduto } = useCarrinhoContext();
+  const { removerProduto } = useCarrinhoContext();
+  const { removerProdutoCarrinho } = useCarrinhoContext();
+
   return (
     <li>
       <>
--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -2,32 +2,12 @@ import React, { useContext } from "react";
 import Produto from "./Produto";
 import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
-import { CarrinhoContext } from "@/context/carrinhoContext";
+import {useCarrinhoContext} from "@/hooks/useCarrinhoContext"
 
 const Produtos = () => {
-  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
-
-  function adicionarProduto(novoProduto) {
-    const temProduto = carrinho.some((itemDoCarrinho) => {
-      itemDoCarrinho.id === novoProduto.id;
-    });
-
-    if (!temProduto) {
-      novoProduto.quantidade = 1;
-      return setCarrinho((carrinhoAnterior) => [
-        ...carrinhoAnterior,
-        novoProduto,
-      ]);
-    }
-    setCarrinho((carrinhoAnterior) =>
-      carrinhoAnterior.map((itemDoCarrinho) => {
-        if (itemDoCarrinho.id === novoProduto.id)
-          itemDoCarrinho.quantidade += 1;
-        return itemDoCarrinho;
-      })
-    );
-  }
-
+  const {adicionarProduto} = useCarrinhoContext();
+  const {removerProduto} = useCarrinhoContext()
+  
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
@@ -37,6 +17,8 @@ const Produtos = () => {
             key={produto.id}
             {...produto}
             adicionarProduto={adicionarProduto}
+            removerProduto = {removerProduto}
+             
           />
         ))}
       </div>
